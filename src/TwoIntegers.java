a83f64a6f175c0b54425c5a860aaee366ccf3f3a
bob <bob@APCSA>
2018-09-06 15:37:26 -0700

    I imported and created a TwoInteger class.

---
+++ src/TwoIntegers.java
@@ -0,0 +1,17 @@
+public class TwoIntegers 
+{
+	private int number1;
+	private int number2;
+	
+	public TwoIntegers()
+	{
+		number1 = 0;
+		number2 = 0;
+	}
+	public TwoIntegers(int number1, int number2)
+	{
+		this.number1 = number1;
+		this.number2 = number2;
+	}
+	
+}
