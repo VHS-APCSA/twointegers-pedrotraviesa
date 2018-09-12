d27037e969051c6fd656bd13fbada2205383ec14
bob <bob@APCSA>
2018-09-12 14:52:37 -0700

    Created an arithmetic method to find the sum, difference, product, quotient, and mod of nunber1 and number2

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -13,5 +13,30 @@ public class TwoIntegers
 		this.number1 = number1;
 		this.number2 = number2;
 	}
-	
+	public int getNumber1()
+	{
+		return number1;
+	}
+	public int getNumber2()
+	{
+		return number2;
+	}
+	public void setNumber1(int number1)
+	{
+		this.number1 = number1;
+	}
+	public void setNumber2(int number2)
+	{
+		this.number2 = number2;
+	}
+	public String arithmetic()
+	{
+		int sum = number1 + number2;
+		int product = number1 * number2;
+		int difference = number1 - number2;
+		double qoutient = (double)number1 / number2;
+		int mod = number1 % number2;
+		
+		return (number1 + " + " + number2 + " = " + sum)
+	}
 }
